da1a3a616e7cab83daf091df4bbcc5cd2d846061
Reistoge <64452194+Reistoge@users.noreply.github.com>
2025-12-15 22:51:22 -0300

    updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173,
+    allowedHosts: [
+      'frontend-production-9824.up.railway.app',
+      'localhost',
+      '127.0.0.1',
+    ],
+  },
   server: {
     port: 5173,
     proxy: {
