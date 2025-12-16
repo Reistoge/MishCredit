3047591d384a0ab9c6b4983fd18b58b26e31c204
Reistoge <64452194+Reistoge@users.noreply.github.com>
2025-12-15 22:56:43 -0300

    Update Dockerfile and vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
+
+  server: {
     port: 5173,
     allowedHosts: [
       'frontend-production-9824.up.railway.app',
       'localhost',
       '127.0.0.1',
     ],
-  },
-  server: {
-    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
@@ -20,6 +18,6 @@ export default defineConfig({
         secure: false,
       },
     },
-  
+
   },
 });
